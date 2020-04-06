e0fc2c36d3a083b8740a51173e5d83e32cf440dc
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-06 18:29:22 -0500

    isadmin field added

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true, trim: true },
-  isAdmin: { Boolean },
+  isAdmin: Boolean,
 });
 
 // const userSchema = new mongoose.Schema({
