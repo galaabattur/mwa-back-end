36b60c5af1c23386ad8236dcc357a6964dab2dcc
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-17 23:55:27 -0500

    database changed

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
 var mongoose = require("mongoose");
+var mongodb_connection_string =
+  (process.env.MONGODB_URI || "mongodb://localhost:27017/wishlist-app") +
+  "heroku_6k9gks3m";
 
 mongoose
-  .connect("mongodb://localhost:27017/wishlist-app", {
+  .connect(this.mongodb_connection_string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
   .then(() => {
-    console.log("connected");
+    console.log("MongoDB Connected");
   })
   .catch((err) => console.log("Error", err));
 
